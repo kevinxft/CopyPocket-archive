964dd56b30a84ab1d7430028221785bc655b54af
kevinxft <kevinxft@gmail.com>
2024-09-29 21:32:54 +0800

    research， 能满足我的需求

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,3 +1,16 @@
 export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
+  console.log("Hello background!", { id: browser.runtime.id });
+  chrome.contextMenus.create({
+    id: "SaveToCopySaver",
+    title: "保存到CopySaver",
+    contexts: ["link", "selection"],
+  });
+
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "copyToSaver") {
+      console.log("复制到 Saver 被点击");
+      console.log("链接地址:", info.linkUrl);
+      console.log("选中的文本:", info.selectionText);
+    }
+  });
 });
